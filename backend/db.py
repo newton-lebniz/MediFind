82d1a00c551f16dfe17d1d1eb3f39b0ea0be4530
Bhargav Prasanna <ubhargavprasanna@gmail.com>
2026-03-23 19:43:05 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,30 @@
+import mysql.connector
 
+def get_connection():
+    return mysql.connector.connect( 
+        host="localhost",
+        user="root",
+        password="Bhargav@2008",
+        database="medifind"
+    )
+def get_doctors_by_specialization(specialization):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    query = """
+   SELECT d.*
+    FROM doctors d
+    JOIN specializations s ON d.specialization_id = s.specialization_id
+    WHERE LOWER(s.specialization_name) = LOWER(%s)
+    ORDER BY d.rating DESC
+    LIMIT 5
+   
+    """
+
+    cursor.execute(query, (specialization,))
+    result = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return result
