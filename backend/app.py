38da8f538b254f9cbb580a1d50925d00496f2204
Bhargav Prasanna <ubhargavprasanna@gmail.com>
2026-04-15 14:48:56 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db import Base, engine
 from routes import router
 
-# Create tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
