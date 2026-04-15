3acbb17f58468bfa5db08c7c474a66139ff6e208
Bhargav Prasanna <ubhargavprasanna@gmail.com>
2026-04-15 12:18:28 +0530

    Update chatbot.py

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key ="AIzaSyB-CXbRUKUl4YRe3BqwB0YQ_2QgB838r_c"
+openai.api_key ="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"
 
 def ask_chatbot(query):
     response = openai.ChatCompletion.create(
