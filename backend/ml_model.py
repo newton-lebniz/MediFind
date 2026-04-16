5fc12983b57c0ccf9e7b5b66409c940b7eb7e6c6
Bhargav Prasanna <ubhargavprasanna@gmail.com>
2026-04-16 20:53:50 +0530

    Update ml_model.py

--- backend/ml_model.py
+++ backend/ml_model.py
@@ -1,18 +1,19 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
-ml = LinearRegression()
+model = LinearRegression()
 
 def train_model(dataset):
     if len(dataset) < 2:
         return
+
     X = np.array([[d["rating"], d["distance"]] for d in dataset])
     y = np.array([d["rating"] / (d["distance"] + 0.0001) for d in dataset])
-    ml.fit(X, y)
+
+    model.fit(X, y)
 
 def predict_score(rating, distance):
     try:
-        return ml.predict([[rating, distance]])[0]
+        return model.predict([[rating, distance]])[0]
     except:
         return rating / (distance + 0.0001)
-
