51da343e612775517a0f70b78de30794d5bd19c1
Bhargav Prasanna <ubhargavprasanna@gmail.com>
2026-04-16 20:53:24 +0530

    Update crud.py

--- backend/crud.py
+++ backend/crud.py
@@ -2,11 +2,14 @@ from sqlalchemy.orm import Session
 from models import Hospitals
 
 def insert_hospital(db: Session, data):
-    existing = db.query(Hospitals).filter(Hospitals.name == data["name"]).first()
+    existing = db.query(Hospitals).filter(
+        Hospitals.hospital_name == data["hospital_name"],
+        Hospitals.latitude == data["latitude"],
+        Hospitals.longitude == data["longitude"]
+    ).first()
 
     if existing:
         return
 
-    hospital = Hospitals(**data)
-    db.add(hospital)
+    db.add(Hospitals(**data))
     db.commit()
